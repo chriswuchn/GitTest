3ef343ff5fde47b8b1a4b0130e7d6a6f18ed76a6
chris <chriswuchn@gmail.com>
2020-10-13 23:18:58 +0800

    first commit

---
+++ src/test/java/TestGit.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+/**
+ * @author chris
+ * @create 2020-10-13-{22:04}
+ */
+
+
+public class TestGit {
+
+    @Test
+    public void testHello(){
+
+        System.out.println("Hello");
+    }
+
+}
